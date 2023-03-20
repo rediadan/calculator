a292598b79133d37e3d24ca89479dd4c900ffef5
rediadan <wlfh4002@gmail.com>
2023-03-20 15:30:12 +0900

    test

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from PyQt5.QtWidgets import (QApplication,QWidget,QPushButton,QVBoxLayout,
                              QMessageBox, QPlainTextEdit)
 from PyQt5.QtGui import QIcon
 
-
+#a
 class Calculator(QWidget):
     
     def __init__(self):
